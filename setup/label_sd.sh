bf264cf7caf2947adf6387d66c03e9779ef87203
Colton Gerber <zaroyu@gmail.com>
2020-09-27 11:15:04 -0500

    Fix conditional to not check if ion in ionarray, remove leftover and unused code from label_ions, change oxygen labeling from 4 to 8 atoms

--- setup/label_sd.sh
+++ setup/label_sd.sh
@@ -2,56 +2,29 @@
 
 # PARAMETERS:
 # 1 = number of ions
-# 2 = ion element (for label)
-# 3 = file to label (e.g. POSCAR or CONTCAR)
+# 2 = file to label (e.g. POSCAR or CONTCAR)
 
-SEQUENTIAL=false
-while getopts "s" OPTION; do
-	case $OPTION in
-	s)
-		SEQUENTIAL=true
-		shift
-		;;
-	*)
-		echo "Incorrect options provided"
-		;;
-	esac
-done
 
-file_to_label="${3}"
+file_to_label="${2}"
 tmpfile=$(mktemp)
 
-if ! "${SEQUENTIAL}"; then
-	cp "${file_to_label}" "$tmpfile" && gawk -v numions="${1}" -v ionelement="${2}" '
-		/Direct/ {
-			print; 
-			for (i = 1; i <= numions; i++) {
-				if (i in ionarray) {
-					getline;
-					printf "%s", $0;
-					printf "%s\n", " T T T" ;
-				}
-			}
-			for (i = 1; i <= 4; i++) {
-				getline;
-				printf "%s", $0;
-				printf "%s\n", " F F F" ;
-			}
+cp "${file_to_label}" "$tmpfile" && gawk -v numions="${1}" '
+	/Direct/ {
+		print;
+		for (i = 1; i <= numions; i++) {
+			getline;
+			printf "%s", $0;
+			printf "%s\n", " T T T" ;
+		}
+		for (i = 1; i <= 4; i++) {
+			getline;
+			printf "%s", $0;
+			printf "%s\n", " F F F" ;
+		}
 
-			for (i = 1; i <= 4; i++) {
-				getline;
-				printf "%s", $0;
-				printf "%s\n", " F F F" ;
-			}
-		next}1' "$tmpfile" >"${file_to_label}"
-else
-	cp "${file_to_label}" "$tmpfile" && gawk -v numions="${1}" -v ionelement="${2}" '
-		/Direct/ {
-			print; 
-			for (i = 1; i <= numions; i++) {
-				getline;
-				printf "%s", $0;
-				printf "%s\n", " # " ionelement " " i;
-			}
-		next}1' "$tmpfile" >"${file_to_label}"
-fi
+		for (i = 1; i <= 4; i++) {
+			getline;
+			printf "%s", $0;
+			printf "%s\n", " F F F" ;
+		}
+	next}1' "$tmpfile" >"${file_to_label}"
